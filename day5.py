6633e0157df2d4a2bd8fa3e585a4f6b60cde1006
Tarun <talaganatarun123@gmail.com>
2026-03-24 12:04:14 +0530

    Updated Code

--- day5.py
+++ day5.py
@@ -1,4 +1,7 @@
 #string --> string is a collection of char
 # k=int(input())
 any="Python programming language"
-any[7]="0"
+#any[7]="0"
+# we cannot perform the modification because strin it not mutable
+any="java"
+print(any)
