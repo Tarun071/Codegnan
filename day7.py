c066b90a7930389df41c51e7c734a09ed11a650c
Tarun <talaganatarun123@gmail.com>
2026-03-28 12:57:47 +0530

    updated code

---
+++ day7.py
@@ -0,0 +1,79 @@
+#dictionary
+'''
+we can store data as    key : value
+keys - are union and we can only give immutable data types in keys
+values - we all data types (any values are acceptable no restrictions)
+'''
+
+
+'''
+METHODS :
+-------
+ keys() --- this method is used to get all keys from dictionary
+ values() --- this method is used to get all values from the dictionary
+ clear() -- this method is used to clear the dictionary (empty the dictionary)
+
+sbi_teja={
+        "name":"Teja",
+        "Role":"Mentor",
+        "sal":50000
+    }
+
+print(sbi_teja.keys())
+print(sbi_teja.values())
+print(sbi_teja["name"])
+'''
+
+
+#set
+'''
+set{}--- set data type is a unordered collection and it never allow duplicates
+METHODS :
+---------
+union() --- this is used to add or get 2 different sets without duplicated
+intersection() --- gives commom elements
+difference () --- gives common element from first set
+pop() -- removes element from startingv
+
+
+any={1,1,2,3,4,3}
+so={4,3,5,6}
+print(any)
+print(so)
+print(any.union(so))
+print(any.intersection(so))
+print(any.difference(so))
+print(so.difference(any))
+any.pop()
+print(any)
+'''
+
+
+#check whether user input pin is correct or not
+
+user=input()
+pas=input()
+
+db={
+    "tarun":"0613"
+}
+
+if pas in db[user]:
+    print("Right credentials")
+else:
+    print("Wrong credentials")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
