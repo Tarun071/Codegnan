8c993e85f143306730ca0dbc525bc87a7960298f
Tarun <talaganatarun123@gmail.com>
2026-04-01 12:59:10 +0530

    day10

---
+++ day10.py
@@ -0,0 +1,84 @@
+n=int(input())
+k=n
+sum=1
+for i in range(1,n):
+    if (n%i==0):
+        sum=sum*i
+if sum==k:
+    print("perfect square")
+else:
+    print("not a perfetct square")
+
+#sum of evens and odd in list
+'''li=list(map(int,input().split()))
+even=0
+odd=0
+for i in li:
+    if i%2==0:
+        even+=i
+    else:
+        odd+=i
+print(even)
+print(odd)'''
+
+
+#table
+'''
+n=int(input())
+for i in range(1,11):
+     print(n,"*",i,"=",n*i)
+'''
+
+#finding count upper and lower letters in string
+'''
+string1=input()
+upper=0
+lower=0
+for i in string1:
+    if i.isupper():
+        upper+=1
+    elif i.islower():
+        lower+=1
+print(upper)
+print(lower)'''
+
+
+'''
+string1=input()
+upper=[]
+lower=[]
+for i in string1:
+    if i.isupper():
+        upper.append(i)
+    elif i.islower():
+        lower.append(i)
+print(upper)
+print(lower )
+'''
+
+#ATM machine validation
+'''
+ICIC_Raghu_AC_details={"Name":"Raghu","ATM PIN": "9100"}
+print("Welcome to ICIC ATM")
+print("Pls insert your ATM card")
+ICIC_user_pin=input("Pls enter 4 digits ATM pin:")
+if len(ICIC_user_pin)==4:
+    if ICIC_user_pin in ICIC_Raghu_AC_details["ATM PIN"]:
+        print("The pin is correct")
+    else:
+        print("You have entered invalid pin")
+else:
+    print("Plz enter 4 digit pin")'''
+
+
+
+#perfect number
+n=int(input())
+sum=0
+for i in range(1,n):
+    if n%i==0:
+        sum+=i
+if sum==n:
+    print(n,"is a perfect number")
+else:
+    print(n,"is not a perfect number")
--- rough.py
+++ rough.py
@@ -1,1 +1,9 @@
-   
+a=int(input())
+b=int(input())
+c=int(input())
+if a>b and a>c:
+    print(a,"is greatest number")
+elif b>a and b>c:
+    print(b,"is greatest number")
+elif c>a and c>b:
+    print(c,"is greatest number")
