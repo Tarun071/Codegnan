5f7f089203c810b2d8ac0a43dde960b63194d089
Tarun <talaganatarun123@gmail.com>
2026-04-10 15:56:33 +0530

    practice

--- rough.py
+++ rough.py
@@ -7,10 +7,26 @@ elif b>a and b>c:
     print(b,"is greatest number")
 elif c>a and c>b:
     print(c,"is greatest number")
-'''
+
 w=input()
 count=1
 for i in w:
     if i==" ":
         count+=1
-print(count)
+print(count)'''
+
+
+#list of even numbers
+def check():
+    
+    l=[]
+    for i in a:
+        if i%2==0:
+            l.append(i)
+    if len(l)==0:
+        print("No even numbers ")
+    else:
+        print(" even numbers are : ",l)
+
+
+a=[1,3,5] 
