9c09a80072ef8ebb4d28f4104c70bc721175c7e8
Tarun <talaganatarun123@gmail.com>
2026-04-28 12:21:47 +0530

    up

--- day24.py
+++ day24.py
@@ -30,12 +30,64 @@ print(so)
 syntax--> re.findall("metchar),var_name
 '''
 
+# import re 
+# any="this is a method can used to read entire file and this can be done"
+# we="hello"
+# so=re.findall("[a-z]",any)
+# an=re.search("[a]",any)
+# the=re.findall("h...o",we)
+# the2=re.search("h...o",we)
+# print(the)
+# print(the2)
+
+# import re
+# any="This meta character"
+# an=re.findall("Th.?",any)
+# print(an)
+
 import re 
-any="this is a method can used to read entire file and this can be done"
-we="hello"
-so=re.findall("[a-z]",any)
-an=re.search("[a]",any)
-the=re.findall("h...o",we)
-the2=re.search("h...o",we)
-print(the)
-print(the2)
+any="This is meta character will form"
+an=re.findall("that|will",any)
+print(an)
+
+
+'''
+#special sequence
+-----------------
+A special sequence is a \ followed by one of the characters in the list below,and has a
+
+special meaning
+
+\A
+----
+Returns a match if the specified characters are at the beginning of the string
+Eg: "\AThe"
+'''
+# import re
+# txt="The rain in Spain"
+# #Check if the string starts with the "The":
+# x=re.findall("\AThe",txt)
+# print(x)
+# if x:
+#     print("Yes,there is a match!")
+# else:
+#     print("No Match")
+
+
+
+'''
+\b-Returns match where the specified characters are at the beginning or at the end of a word
+Eg: r"\bain "
+
+import re
+txt="The rain in Spain"
+#Check if the string starts with the "The":
+x=re.findall("\bSpain",txt)
+print(x)
+if x:
+    print("Yes,there is a match!")
+else:
+    print("No Match")
+
+
+'''
